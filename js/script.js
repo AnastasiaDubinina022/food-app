1d08550cfeb4f39d62ddec9bc38ca09304472818
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-05-16 14:39:58 +0300

    Modal window is modified

--- js/script.js
+++ js/script.js
@@ -115,18 +115,16 @@ window.addEventListener('DOMContentLoaded', () => {
     const modal = document.querySelector('.modal');
 
     function openModal() {
-        modalTrigger.forEach((item) => {
-            item.addEventListener('click', () => {
-                //modal.classList.toggle('show');
-                modal.classList.add('show');
-                modal.classList.remove('hide');
-                document.body.style.overflow = 'hidden';
-            });
-        });
-        
+        //modal.classList.toggle('show');
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden'; 
+        clearInterval(modalTimerId); // ниже есть сеттаймаут открывающий модалку через 5 сек как пользователь зашел. Здесь мы его очищаем в том случае, если пользователь еще раньше сам открыл модалку по нажатию кнопки и уже его видел, чтобы повторно оно не выскакивало и не бесило.
     }
 
-    openModal();
+    modalTrigger.forEach((item) => {
+        item.addEventListener('click', openModal);
+    });
 
     function closeModal() {
         //modal.classList.toggle('show');
@@ -147,11 +145,20 @@ window.addEventListener('DOMContentLoaded', () => {
         if (event.code === 'Escape' && modal.classList.contains('show')) {
         closeModal();
     } // если нажата клавиша esc при открытом модальном окне, то тогда вызываем функцию закрытия, при закрытом окне она не будет срабатывать.
-});
+    });
 
+    const modalTimerId = setTimeout(openModal, 5000); // открываем модалку через 5 сек после захода пользователя на страницу
+    
+    function showModalByScroll() {
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
 
-    
-    
+        } // window.scrollY = window.pageYOffset (depricated)
+          // document.documentElement.scrollHeight -1 = в конце добавляем -1px иначе не срабатывает - это баг, возможно связанный с некоторыми версиями браузеров или ограничениями некоторых мониторов. т.е. окно срабатывает на 1 px раньше самой нижней точки прокрутки, в этом ничего страшного.
+    } 
+
+    window.addEventListener('scroll', showModalByScroll);
 
 });
 
