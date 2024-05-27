2d0818969164c93bb746210ac054f4db1e11f6fb
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-05-27 13:01:53 +0300

    Modified menuCards (used ...rest classes)

--- js/script.js
+++ js/script.js
@@ -163,12 +163,13 @@ window.addEventListener('DOMContentLoaded', () => {
     // используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes; // не забывать что это массив
             this.parent = document.querySelector(parentSelector); // получаем селектор, куда будем вставлять нашу верстку, можно сделать это и в методе рендер
             this.transfer = 90; // пока пишем фиксированный курс доллара к рублю
             this.changeToRUB(); // можем здесь вызвать метод конвертации валюты, на моменте конструирования, или можем тоже в рендере сделать это
@@ -181,17 +182,21 @@ window.addEventListener('DOMContentLoaded', () => {
         render() {
             const element = document.createElement('div');
 
-            console.log(this.src);
+            if (this.classes.length === 0 || !this.classes.includes('menu__item')) { // если ни один класс не передан или не передан дефолтный
+                this.classes = 'menu__item';          // то присваиваем класс по умолчанию 
+                element.classList.add(this.classes);  // и добавляем его элементу
+            } else {
+                this.classes.forEach(className => element.classList.add(className));  // в остальных случаях каждый переданный в аргументы класс добавляем элементу
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-                    </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
                 </div>
             `;
             this.parent.append(element);
@@ -218,7 +223,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         12,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
     new MenuCard( 
         "img/tabs/post.jpg",
@@ -226,7 +232,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         10,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render(); 
 });
 
