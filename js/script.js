1b6a2fc6612cda5de49b53d2a25645c1e464e053
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-07-15 17:16:20 +0300

    Calculator script added.

--- js/script.js
+++ js/script.js
@@ -518,5 +518,79 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+
+    // Calc
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female'; // 
+    let ratio = 1.375;  // задаем значения по умолчанию
+    let height, weight, age;
+
+    function calcTotal() {   // будет считать результат по формуле. будем вызывать ее каждый раз, когда пользователь меняет данные, чтобы нормально пересчитать значение
+        if (!sex || !height || !weight || !age || !ratio) {   // начинаем с проверки, если хотя бы одно поле не заполнено (имеет false в лог.контексте)
+            result.textContent = '____';        // выводим сообщение, напр. 'Заполните все поля'
+            return;                                           // прерываем функцию, рассчеты проводиться не будут.
+        }
+
+        if (sex == 'female') {   // формула для женщин
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        }
+
+        if (sex == 'male') {     // формула для мужчин
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {    // получение информации со статических блоков (пол и активность, 1 функция т.к. они одинаковые)
+        const elements = document.querySelectorAll(`${parentSelector} div`);   // получаем все дивы внутри родителя
+
+        elements.forEach((elem) => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {         // если клик на блок с дата-атрибутом активности
+                    ratio = +e.target.getAttribute('data-ratio');  // берем оттуда коэффициент
+                } else {                                 // подразумеваем клик на блок с выбором пола
+                    sex = e.target.getAttribute('id');   // записываем оттуда пол 
+                }
+    
+                elements.forEach((elem) => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                e.target.classList.add(activeClass);
+    
+                calcTotal();
+            });
+        });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getInputInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch (input.getAttribute('id')) {
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+
+            calcTotal();
+        })
+    }
+
+    getInputInformation('#weight');
+    getInputInformation('#height');
+    getInputInformation('#age');
+
 });                                           
 
