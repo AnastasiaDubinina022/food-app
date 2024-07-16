c443e1bbd7e0c4f54530966c662e54a52c9fbb2b
Anastasia Dubinina <anastasiadubinina1123@gmail.com>
2024-07-16 14:59:11 +0300

    Calculator modified. Сheck for entering letters into inputs and signals. Saving data to local storage.

--- js/script.js
+++ js/script.js
@@ -522,9 +522,40 @@ window.addEventListener('DOMContentLoaded', () => {
     // Calc
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female'; // 
-    let ratio = 1.375;  // задаем значения по умолчанию
-    let height, weight, age;
+    let sex, height, weight, age, ratio;
+
+    if (localStorage.getItem('sex')) {     // устанавливаем настройки если пользователь уже заходил и они есть в локал сторэдж
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
+
+    function initLocalSettings(selector, activeClass) {       // добавляет класс активности блокам, сохраненным в локал сторэдж
+        const elements = document.querySelectorAll(selector); // получаем все блоки внутри селектора родителя (ниже вызываем с '#gender div')
+
+        elements.forEach((elem) => {             // перебираем блоки до совпадения
+            elem.classList.remove(activeClass);
+
+            if (elem.getAttribute('id') === localStorage.getItem('sex')) {  // когда значение айди в блоке и в локал сторэдж совпадет
+                elem.classList.add(activeClass);           // этому блоку вешаем класс активности
+            }
+
+            if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {  // тоже по дата-атрибуту для коэффициента активности
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
 
     function calcTotal() {   // будет считать результат по формуле. будем вызывать ее каждый раз, когда пользователь меняет данные, чтобы нормально пересчитать значение
         if (!sex || !height || !weight || !age || !ratio) {   // начинаем с проверки, если хотя бы одно поле не заполнено (имеет false в лог.контексте)
@@ -543,15 +574,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     calcTotal();
 
-    function getStaticInformation(parentSelector, activeClass) {    // получение информации со статических блоков (пол и активность, 1 функция т.к. они одинаковые)
-        const elements = document.querySelectorAll(`${parentSelector} div`);   // получаем все дивы внутри родителя
+    function getStaticInformation(selector, activeClass) {    // получение информации со статических блоков (пол и активность, 1 функция т.к. они одинаковые)
+        const elements = document.querySelectorAll(selector);   // получаем все дивы внутри родителя
 
         elements.forEach((elem) => {
             elem.addEventListener('click', (e) => {
                 if (e.target.getAttribute('data-ratio')) {         // если клик на блок с дата-атрибутом активности
                     ratio = +e.target.getAttribute('data-ratio');  // берем оттуда коэффициент
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));  // сохраняем его в локал сторэдж
                 } else {                                 // подразумеваем клик на блок с выбором пола
                     sex = e.target.getAttribute('id');   // записываем оттуда пол 
+                    localStorage.setItem('sex', e.target.getAttribute('id'))  // сохраняем выбранный пол в локал сторэдж
                 }
     
                 elements.forEach((elem) => {
@@ -565,13 +598,19 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    getStaticInformation('#gender', 'calculating__choose-item_active');
-    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
     function getInputInformation(selector) {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+            if (input.value.match(/\D/g)) {
+                input.style.border = '1px solid red';
+            } else {
+                input.style.border = 'none';
+            }
+
             switch (input.getAttribute('id')) {
                 case 'weight':
                     weight = +input.value;
